193de572887df58da64590d820468f97dd5f2181
GuHaoAWA <1441857228@qq.com>
2024-02-22 18:29:29 +0800

    what

---
+++ src/main/java/com/guhao/star/Config.java
@@ -0,0 +1,49 @@
+package com.guhao.star;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.*;
+
+public class Config {
+
+    public final static Map<Item,Float> modelMap = new HashMap<>();
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> SHIELD_ITEM = BUILDER
+            .comment("items considered as shield", "example: minecraft:bow")
+            .defineListAllowEmpty(Collections.singletonList("shield_items"), ArrayList::new, Config::validateItemName);
+
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> BOW_ITEM = BUILDER
+            .comment("items considered as bow", "example: minecraft:bow")
+            .defineListAllowEmpty(Collections.singletonList("bow_items"), ArrayList::new, Config::validateItemName);
+
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> CROSSBOW_ITEM = BUILDER
+            .comment("items considered as crossbow", "example: minecraft:bow")
+            .defineListAllowEmpty(Collections.singletonList("crossbow_items"), ArrayList::new, Config::validateItemName);
+
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> TRIDENT_ITEM = BUILDER
+            .comment("items considered as trident", "example: minecraft:bow")
+            .defineListAllowEmpty(Collections.singletonList("trident_items"), ArrayList::new, Config::validateItemName);
+
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> KATANA_ITEM = BUILDER
+            .comment("items considered as katana", "example: minecraft:bow 0")
+            .defineListAllowEmpty(Collections.singletonList("katana_items"), ArrayList::new, obj -> true);
+
+    public static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    private static boolean validateItemName(final Object obj)
+    {
+        return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(new ResourceLocation(itemName));
+    }
+    public static void load(){
+        List<? extends String> katanaitem = Config.KATANA_ITEM.get();
+        for(String katana : katanaitem){
+            String[] entry = katana.split(" ");
+            Item katanaItem = ForgeRegistries.ITEMS.getValue(new ResourceLocation(entry[0]));
+            modelMap.put(katanaItem, Float.parseFloat(entry[1]));
+        }
+    }
+
+}
---
+++ src/main/java/com/guhao/star/regirster/Items.java
@@ -0,0 +1,14 @@
+package com.guhao.star.regirster;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static com.guhao.star.Star.MODID;
+
+public class Items {
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+        public static final RegistryObject<Item> CUSTOM_SHEATH = ITEMS.register("custom_sheath", () -> new Item(new Item.Properties().rarity(Rarity.EPIC)));
+    }
+
